d65a192422b19c8c2e3996954fdc98d00c6c44b6
NgDuyPhong <NgDuyPhong@users.noreply.github.com>
2021-07-01 09:07:57 +0700

    Phong

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FacebookGuard } from './guards/facebook.guard';
 import { HelloComponentComponent } from './hello-component/hello-component.component';
 
 const routes: Routes = [
-  { path: 'hello-component', component: HelloComponentComponent },
+  {
+    path: '',
+    // loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule),
+    component: HomeComponent,
+    canActivate: [FacebookGuard]
+  },
+  {
+    path: 'hello-component',
+    component: HelloComponentComponent },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponentComponent } from './hello-component/hello-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponentComponent
+    HelloComponentComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
